3e076d11a0b8683189d05b7c20ebda6fb4ede7d3
DariusCerniavskis <Darius.Cerniavskis@gmail.com>
2026-01-07 21:31:22 +0200

    All tasks

---
+++ 01-07popiete/script.js
@@ -0,0 +1,38 @@
+const cryptosWrapper = document.getElementById("cryptos-wrapper");
+
+const buildScreen = async () => {
+    const response = await fetch(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1"
+    );
+
+    const unsortedCryptos = await response.json();
+
+    const sortedCryptos = unsortedCryptos.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+    });
+
+    console.log(sortedCryptos);
+
+    sortedCryptos.forEach((crypto, index) => {
+        const card = document.createElement("div");
+
+        const borderColor = `${
+            crypto.current_price > 100 ? "gold" : "silver"
+        }Border`;
+        card.classList.add("crypto-wrapper", borderColor);
+
+        const title = document.createElement("h1");
+        title.classList.add("title");
+        title.innerText = crypto.name;
+
+        const currPrice = document.createElement("h2");
+        currPrice.classList.add("currPrice");
+        currPrice.innerText = `$${Number(crypto.current_price).toFixed(6)}`;
+
+        card.append(title, currPrice);
+
+        cryptosWrapper.append(card);
+    });
+};
+
+buildScreen();
---
+++ 01-07rytas/script.js
@@ -0,0 +1,47 @@
+const topGamesWrapper = document.getElementById("top-games-wrapper");
+
+const buildScreen = async () => {
+    const response = await fetch(
+        "https://695e14ac2556fd22f6773e58.mockapi.io/topGames"
+    );
+
+    const topGames = await response.json();
+    console.log(topGames);
+
+    topGames.forEach((game) => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        const image = document.createElement("img");
+        image.classList.add("image");
+        image.src = game.image;
+
+        const title = document.createElement("h1");
+        title.classList.add("title");
+        title.innerText = game.name;
+
+        const realiseAndPlatforms = document.createElement("div");
+        realiseAndPlatforms.classList.add("real-plat-wrapper");
+
+        const realiseDate = document.createElement("h4");
+        realiseDate.classList.add("realise-date");
+        realiseDate.innerText = game.RealiseYear;
+
+        const platforms = document.createElement("h4");
+        platforms.classList.add("platforms");
+        platforms.innerText = game.platforms;
+
+        const disciption = document.createElement("p");
+        disciption.classList.add("discription");
+        disciption.innerText = game.description;
+
+        card.append(image, title, realiseAndPlatforms, disciption);
+        realiseAndPlatforms.append(realiseDate, platforms);
+
+        // console.log(topGamesWrapper);
+
+        topGamesWrapper.append(card);
+    });
+};
+
+buildScreen();
